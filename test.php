3c7bf3b173b7c940c22c07f2aa62951c157f506f
Martijn <martijn.niji@gmail.com>
2013-07-17 12:58:06 +0200

    Initial commit

---
+++ test.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+
+<?php
+
+include "git4php.php";
+
+$repodir = '/home/klm23563/projects/git4php/testrepo/';
+$sha = Git::getHeadSha($repodir);
+$type = GitObject::TYPE_COMMIT;
+
+$go = new GitCommit($repodir, $sha);
+
+//echo "Root sha: ".$go->getSha1()."\n";
+//echo "Tree sha: ".$go->getTree()."\n";
+$tree = new GitTree($repodir, $go->getTree());
+echo "Tree content: ".$tree->getContent()."\n";
+
+while ($go->getParent() !== false) {
+    $go = new GitCommit($repodir, $go->getParent());
+    //echo "Child sha: ".$go->getSha1()."\n";
+}
