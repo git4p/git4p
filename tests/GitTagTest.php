7dfd9ae0e53493c9678fa8a3132e1c5d19007fab
Martijn <martijn.niji@gmail.com>
2015-06-11 23:26:28 +0200

    Add basic tests for tags

---
+++ tests/GitTagTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Git4p\Git;
+use Git4p\GitObject;
+use Git4p\GitTag;
+
+class GitTagTest extends PHPUnit_Framework_TestCase {
+
+    protected $git    = false;
+    protected $gittag = false;
+
+    public function setup() {
+        $this->git    = new Git('/tmp/phpunit/gittestrepo');
+        $this->gittag = new GitTag($this->git);
+    }
+
+    public function teardown() {
+        $this->git    = false;
+        $this->gittag = false;
+    }
+
+    public function testShouldReturnGitTagType() {
+        $this->assertEquals($this->gittag->type(), GitObject::TYPE_TAG);
+    }
+
+    public function testShouldHaveTagName() {
+        $this->gittag->setTag('1.0.0');
+        $this->assertEquals($this->gittag->tag(), '1.0.0');
+    }
+
+    public function testShouldReturnFalseOnEmptyMessage() {
+        $this->assertFalse($this->gittag->message());
+    }
+
+    public function testCanHaveMessage() {
+        $this->gittag->setMessage('Some sort of message about the tag.');
+        $this->assertEquals($this->gittag->message(), 'Some sort of message about the tag.');
+    }
+}
