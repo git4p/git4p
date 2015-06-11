fe3dee1f5387b03d3ff159952a1cda3ecb2fe3f0
Martijn <martijn.niji@gmail.com>
2015-06-11 23:48:44 +0200

    Add simple tests for user

---
+++ tests/GitUserTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Git4p\GitUser;
+
+class GitUserTest extends PHPUnit_Framework_TestCase {
+
+    protected $gituser = false;
+
+    public function setup() {
+        $this->gituser = new GitUser();
+        $this->gituser->setName('Some User');
+        $this->gituser->setEmail('some.user@example.com');
+        $this->gituser->setTimestamp('1374058686');
+        $this->gituser->setOffset('+0100');
+    }
+
+    public function teardown() {
+        $this->gituser = false;
+    }
+
+    public function testShouldHaveName() {
+        $this->assertEquals($this->gituser->name(), 'Some User');
+    }
+
+    public function testShouldHaveEmail() {
+        $this->assertEquals($this->gituser->email(), 'some.user@example.com');
+    }
+
+    public function testShouldHaveTimestamp() {
+        $this->assertEquals($this->gituser->timestamp(), '1374058686');
+    }
+
+    public function testShouldHaveOffset() {
+        $this->assertEquals($this->gituser->offset(), '+0100');
+    }
+
+}
