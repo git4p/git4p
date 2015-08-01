8031fffc5c5be23141ba29bdcb4444eb30285b7b
Martijn <martijn.niji@gmail.com>
2015-08-01 19:15:36 +0200

    Make sure test dir is created

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,23 @@
 <?php
 
+function delTree($dir) {
+    $files = array_diff(scandir($dir), array('.','..'));
+    foreach ($files as $file) {
+        (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+    }
+    return rmdir($dir);
+}
+
 ini_set('include_path', ini_get('include_path')
                         .PATH_SEPARATOR.dirname(__FILE__).'/src/');
+
+define('GIT4P_TESTDIR', '/tmp/phpunit/git4p/testrepo');
+
+if (file_exists(GIT4P_TESTDIR)) {
+    delTree(GIT4P_TESTDIR);
+}
+
+mkdir(GIT4P_TESTDIR, 0777, true);
 
 require_once 'Git.php';
 require_once 'GitObject.php';
